f7abaa9ad1df608acf87244014334bead9ea6bb4
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-11-11 19:24:06 +0700

    2.5. Lấy ra chủ đề bài hát

--- index.ts
+++ index.ts
@@ -5,13 +5,21 @@ dotenv.config();
 import { connect } from "./config/database";
 connect();
 
+import { Topic } from "./models/topic.model";
+
 const app: Express = express();
 const port: number = 3000;
 
 app.set('views', `${__dirname}/views`); // Tìm đến thư mục tên là views
 app.set('view engine', 'pug'); // template engine sử dụng: pug
 
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false
+  });
+
+  console.log(topics);
+
   res.render("client/pages/topics/index");
 });
 
