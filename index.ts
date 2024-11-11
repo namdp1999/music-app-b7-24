a2a4a48cd349a7fedb02188d75f5df8ef96f25b7
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-11-11 19:29:36 +0700

    3.1 + 3.2

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ dotenv.config();
 import { connect } from "./config/database";
 connect();
 
-import { Topic } from "./models/topic.model";
+import { routesClient } from "./routes/client/index.route";
 
 const app: Express = express();
 const port: number = 3000;
@@ -13,15 +13,7 @@ const port: number = 3000;
 app.set('views', `${__dirname}/views`); // Tìm đến thư mục tên là views
 app.set('view engine', 'pug'); // template engine sử dụng: pug
 
-app.get("/topics", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false
-  });
-
-  console.log(topics);
-
-  res.render("client/pages/topics/index");
-});
+routesClient(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
---
+++ routes/client/index.route.ts
@@ -0,0 +1,8 @@
+import { Express } from "express";
+import { topicsRoute } from "./topic.route";
+
+export const routesClient = (app: Express) => {
+
+  app.use("/topics", topicsRoute);
+
+}
