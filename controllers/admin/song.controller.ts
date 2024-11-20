adf08e15e028b2babe2721f2c21bcdd840be0c2f
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-11-20 20:30:44 +0700

    5.1. Làm giao diện

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { Song } from "../../models/song.model";
+import { Topic } from "../../models/topic.model";
+import { Singer } from "../../models/singer.model";
 
 export const index = async (req: Request, res: Response) => {
   const songs = await Song.find({
@@ -9,5 +11,21 @@ export const index = async (req: Request, res: Response) => {
   res.render("admin/pages/songs/index", {
     pageTitle: "Quản lý bài hát",
     songs: songs
+  });
+};
+
+export const create = async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false
+  }).select("title");
+
+  const singers = await Singer.find({
+    deleted: false
+  }).select("fullName");
+
+  res.render("admin/pages/songs/create", {
+    pageTitle: "Thêm mới bài hát",
+    topics: topics,
+    singers: singers
   });
 };
