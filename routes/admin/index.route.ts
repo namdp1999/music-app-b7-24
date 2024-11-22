c54887e7736e6b07bd36f6f71b49828cb5b04136
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-11-22 20:17:43 +0700

    4. Upload ảnh từ TinyMCE lên Cloudinary

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -3,6 +3,7 @@ import { dashboardRoute } from "./dashboard.route";
 import { systemConfig } from "../../config/system";
 import { topicRoutes } from "./topic.route";
 import { songRoutes } from "./song.route";
+import { uploadRoutes } from "./upload.route";
 
 export const routesAdmin = (app: Express) => {
 
@@ -13,5 +14,7 @@ export const routesAdmin = (app: Express) => {
   app.use(`/${path}/topics`, topicRoutes);
 
   app.use(`/${path}/songs`, songRoutes);
+
+  app.use(`/${path}/upload`, uploadRoutes);
 
 }
