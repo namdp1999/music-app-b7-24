61b1535cc5bbda9aafaca5c4cbc7534c4dd95a85
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-11-22 19:33:17 +0700

    1.2. Tính năng nghe thử audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -12,3 +12,20 @@ if(uploadImage) {
   });
 }
 // Hết Preview ảnh
+
+// Nghe thử
+const uploadAudio = document.querySelector("[upload-audio]");
+if(uploadAudio) {
+  const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+  const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]");
+  const uploadAudioSource = uploadAudio.querySelector("source");
+
+  uploadAudioInput.addEventListener("change", () => {
+    const file = uploadAudioInput.files[0];
+    if(file) {
+      uploadAudioSource.src = URL.createObjectURL(file);
+      uploadAudioPlay.load();
+    }
+  });
+}
+// Hết Nghe thử
